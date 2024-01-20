f56f635988a5f81f95f284a20974434e9aa9ea25
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2024-01-20 20:28:15 +0530

    initial done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 
 import './App.css'
 import { AllRoutes } from './routes/AllRoutes'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
