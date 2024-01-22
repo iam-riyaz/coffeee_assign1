fd809be30ffd0611884ce4ca029bf562f9757744
iam-riyaz <97458162+iam-riyaz@users.noreply.github.com>
2024-01-22 23:48:28 +0530

    redux persist added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,18 @@
 import { legacy_createStore as createStore, combineReducers } from "redux";
 import { companyReducer } from "./companyReducer";
+import { persistStore,persistReducer } from 'redux-persist';
+import storage from "redux-persist/lib/storage";
+
+const persistConfig= {
+    key:"persist-sotre",
+    storage,
+}
 
 const rootReducer = combineReducers({
   company: companyReducer,
 });
 
-export const store = createStore(rootReducer);
+const persistedReducer=persistReducer(persistConfig,rootReducer)
+
+export const store = createStore(persistedReducer);
+export const persister= persistStore(store)
